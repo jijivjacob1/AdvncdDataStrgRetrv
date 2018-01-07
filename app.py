912de89b121117dfbcf5d13953e0d26d9ee37c06
Jiji Jacob <jijijacob@Jijis-MacBook-Pro.local>
2018-01-07 10:21:15 -0600

    cleaned code

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 import numpy as np
-# import pandas as pd
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 import dateutil.relativedelta
+from dateutil import parser
 
 from flask import Flask, jsonify
 
@@ -20,7 +20,7 @@ Base = automap_base()
 # reflect the tables
 Base.prepare(engine, reflect=True)
 
-# Save references to the invoices and invoice_items tables
+# Save references to the stations and measurements tables
 Station = Base.classes.stations
 Measurement = Base.classes.measurements
 
@@ -49,26 +49,26 @@ def welcome():
         f"/api/v1.0/tobs"
         f"- List of Temperature Observations (tobs) for the previous year<br/>"
 
-        f"/api/v1.0/<start>"
+        f"/api/v1.0/start"
         f"- list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.<br/>"
 
-        f"/api/v1.0/<start>/<end>"
-        f"- Ilist of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.<br/>"
+        f"/api/v1.0/start/end"
+        f"- list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.<br/>"
     )
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-    """Return a list invoice totals by country.
-    Each item in the list is a dictionary with keys `country` and `total`"""
-    # Query all countries from the Invoices table
+    """Query for the dates and temperature observations from the last year."""
+
+    # compute previous year relative to cuurent date
     d1 = datetime.today()
     d2 = d1 - dateutil.relativedelta.relativedelta(months=12)
-    # print(d2.strftime("%Y"))
-    results = session.query(Measurement.date, func.avg(Measurement.tobs))\
+    # query to return previous years average tobs for each date
+    results = session.query(Measurement.date, func.round(func.avg(Measurement.tobs),2))\
             .filter(func.strftime("%Y",Measurement.date) == d2.strftime("%Y"))\
             .group_by(Measurement.date).all()
 
-    # Create a list of dicts with `country` and `total` as the keys and
+    # Create a list of dicts with `date` and  average`tobs` as the keys and values
     tobs = []
     for result in results:
         row = {}
@@ -80,58 +80,56 @@ def precipitation():
 
 @app.route("/api/v1.0/stations")
 def stations():
-    """Return a list invoice totals by country.
-    Each item in the list is a dictionary with keys `country` and `total`"""
-    # Query all countries from the Invoices table
+    """Return a list of stations."""
+    # Query all stations  from the stations table
     results = session.query(Station.id,Station.name).all()
 
-    # Create a list of dicts with `country` and `total` as the keys and
-    # Convert list of tuples into normal list
+    # return list of stations
     station_list = list(np.ravel(results))
 
     return jsonify(station_list)
 
 @app.route("/api/v1.0/tobs")
 def tobs():
-    """Return a list invoice totals by country.
-    Each item in the list is a dictionary with keys `country` and `total`"""
+    """Return a json list of Temperature Observations (tobs) for the previous year"""
+
+    # compute previous year relative to cuurent date
     d1 = datetime.today()
     d2 = d1 - dateutil.relativedelta.relativedelta(months=12)
-    # Query all countries from the Invoices table
-    results = session.query(Measurement.tobs)\
-            .filter(func.strftime("%Y",Measurement.date) == d2.strftime("%Y")).all()
+    # Query avg tobs  for all dates in previous year
+    results = session.query(func.round(func.avg(Measurement.tobs),2))\
+            .filter(func.strftime("%Y",Measurement.date) == d2.strftime("%Y")).group_by(Measurement.date).all()
 
-    # Create a list of dicts with `country` and `total` as the keys and
-    # Convert list of tuples into normal list
-    tobs_list = list(np.ravel(results))
+    #return list of tobs
+    return jsonify(list(np.ravel(results)))
 
-    return jsonify(tobs_list)
+@app.route("/api/v1.0/<start>")
+@app.route("/api/v1.0/<start>/<end>")
+def daily_normals(start,end="None"):
 
-# @app.route("/api/v1.0/<start>")
-# def daily_normals(start):
-#     """Return a list invoice totals by country.
-#     Each item in the list is a dictionary with keys `country` and `total`"""
-#     # Query all countries from the Invoices table
-#     results = session.query(Station.id,Station.name).all()
+    """Return a json list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
 
-#     # Create a list of dicts with `country` and `total` as the keys and
-#     # Convert list of tuples into normal list
-#     station_list = list(np.ravel(results))
+    When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date.
 
-#     return jsonify(station_list)   
+    When given the start and the end date, calculate the TMIN, TAVG, and TMAX for dates between the start and end date inclusive."""
+    
+    #parse and compute start date
+    dt_start = parser.parse(start)
+    min_date = dt_start.strftime("%Y-%m-%d")
+    #parse and compute end date if entered else init to max date available
+    if end != "None":
+        dt_end = parser.parse(end)
+        max_date = dt_end.strftime("%Y-%m-%d")
+    else:
+        max_date = session.query(func.max(Measurement.date))\
+             .all()[0][0]
 
-# @app.route("/api/v1.0/<start>/<end>")
-# def daily_normals(start,end):
-#     """Return a list invoice totals by country.
-#     Each item in the list is a dictionary with keys `country` and `total`"""
-#     # Query all countries from the Invoices table
-#     results = session.query(Station.id,Station.name).all()
+    #calculate the TMIN, TAVG, and TMAX for dates between the start and end date inclusive.
+    result = session.query(func.min(Measurement.tobs), func.round(func.avg(Measurement.tobs),2), func.max(Measurement.tobs)).\
+             filter(Measurement.date >= min_date).filter(Measurement.date <= max_date).all()
 
-#     # Create a list of dicts with `country` and `total` as the keys and
-#     # Convert list of tuples into normal list
-#     station_list = list(np.ravel(results))
-
-#     return jsonify(station_list)   
-
+    #return list
+    return jsonify(result)
+  
 if __name__ == '__main__':
     app.run()
